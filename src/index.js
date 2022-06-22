1621bcadd6d7390a5db9a07132baf38c320a9076
sergioFoll <sergiofollyvi@gmail.com>
2022-06-22 14:51:11 +0200

    Basic Query, Resolver and Mutation

--- src/index.js
+++ src/index.js
@@ -6,19 +6,6 @@ const cors = require('cors')
 
 const QRCode = require('qrcode-svg')
 
-const qr = new QRCode({
-  background: '#ffffff',
-  color: '#000000',
-  container: 'svg-viewbox',
-  content: 'http://github.com/',
-  ecl: 'Q',
-  height: 60,
-  join: true,
-  padding: 0,
-  pretty: true,
-  width: 60
-})
-
 const generateShortCode = length => {
   var result = ''
   var characters = 'abcdefghijklmnopqrstuvwxyz0123456789'
@@ -29,17 +16,91 @@ const generateShortCode = length => {
   return result
 }
 
+let qrcodes = [
+  {
+    id: '1',
+    url: 'google.com',
+    svgCode: '<svg>1</svg>',
+    shortCode: 'cklao',
+    title: 'Yessir',
+    description: 'first description'
+  },
+  {
+    id: '2',
+    url: 'youtube.com',
+    svgCode: '<svg>2</svg>',
+    shortCode: 'asdja',
+    title: 'Sit',
+    description: 'amet'
+  },
+  {
+    id: '3',
+    url: 'github.com',
+    svgCode: '<svg>3</svg>',
+    shortCode: 'asaf',
+    title: 'orem d',
+    description: 'Ipsum'
+  }
+]
+
 //basic schema, using GraphQL
 const typeDefs = gql`
   type Query {
-    hello: String
+    qrcodes: [QRCode!]!
+    qrcode(shortCode: String!): QRCode!
+  }
+
+  type QRCode {
+    id: ID!
+    url: String!
+    svgCode: String!
+    shortCode: String!
+    title: String!
+    description: String
+  }
+  type Mutation {
+    newQRCode(url: String!): QRCode!
   }
 `
 
 //resolver for function for schema fields
+//parameters to resolver functions: (parent, args context, info)
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!'
+    qrcodes: () => qrcodes,
+    qrcode: (parent, args) => {
+      return qrcodes.find(qrcode => qrcode.shortCode === args.shortCode)
+    }
+  },
+  Mutation: {
+    newQRCode: (parent, args) => {
+      let url = args.url
+
+      let qr = new QRCode({
+        background: '#ffffff',
+        color: '#000000',
+        container: 'svg-viewbox',
+        content: url,
+        ecl: 'Q',
+        height: 60,
+        join: true,
+        padding: 0,
+        pretty: true,
+        width: 60
+      })
+
+      let qrcodeValue = {
+        id: String(qrcodes.length + 1),
+        url: url,
+        svgCode: qr.svg(),
+        shortCode: generateShortCode(6),
+        title: 'Solo',
+        description: 'Solo links'
+      }
+
+      qrcodes.push(qrcodeValue)
+      return qrcodeValue
+    }
   }
 }
 
